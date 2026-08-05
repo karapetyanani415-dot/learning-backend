a7f56b0588ccd11af1701b404aaf602633e84182
karapetyanani415-dot <karapetyanani415@gmail.com>
2026-08-05 20:38:47 +0400

    Add ESM version

---
+++ local-modules-assignment/esm/index.js
@@ -0,0 +1,10 @@
+import { add, subtract, multiply } from "./utils/math.js"
+import capitalize from "./utils/strings.js"
+
+console.log(add(5, 3))
+console.log(subtract(10, 4))
+console.log(multiply(6, 7))
+
+console.log(capitalize("hello"))
+
+console.log(import.meta.url)
